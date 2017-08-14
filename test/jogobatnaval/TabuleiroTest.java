53d9d0a1ef7f1a636d41837a1129b64b71d29946
JOSE ROCHEL ANGELO MELO <20141D12GR0505@LABS.GARANHUNS.IFPE>
2017-08-14 11:28:10 -0300

    Adicionando testes

---
+++ test/jogobatnaval/TabuleiroTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogobatnaval;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import static jogobatnaval.Tabuleiro.altura;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 20141D12GR0505
+ */
+public class TabuleiroTest {
+
+    public TabuleiroTest() {
+    }
+
+    @Test
+    public void testObterTamanhoDoTab() {
+    }
+
+    @Test
+    public void testIniciarTamanhosDoTab() {
+    }
+
+    @Test
+    public void testCalcularQtdNavios() {
+    }
+
+    @Test
+    public void testObterNumNavios() {
+    }
+    
+    @Test
+    public void testRetornaTabuleiroVazio() {
+    }
+
+    @Test
+    public void testInserirNavios() {
+    }
+
+    @Test
+    public void testRetornarTabuleiroComNavios() {
+    }
+
+    @Test
+    public void testExibirTabuleiro() {
+    }
+    
+}
